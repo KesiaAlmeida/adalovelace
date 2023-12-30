46e497e2b60f98d9ef0f5fdd8ead54bdb4b0d30b
Késia <134666272+KesiaAlmeida@users.noreply.github.com>
2023-12-30 15:06:08 +0000

    Introdução a Javascript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+if(document.getElementById("nome")campo de nome estiver preenchido && campo de email estivere preenchido){
+envie os dados
+}else{
+    peca a usuaria que preecha os campos nome e email
+}
+senão 
+Peça 
